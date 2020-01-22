e238f120adabab936ab1281eea938bc761fb33ad
ntheanh201 <ntheanh201@gmail.com>
2020-01-22 09:53:07 +0700

    Done route

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ntheanh201-react-boilerplate
-Boilerplate for react
+### Web Demo Sessions
+
+##1. Change config file: ./src/environments/index.js
+
+##2. To run development: yarn development
+
+##3. To build: yarn build
---
+++ src/packages/ui/index.js
@@ -0,0 +1,12 @@
+export { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary'
+export {
+  ToggleSwitch,
+  ToggleText,
+  ToggleWrapper
+} from './components/ToggleSwitch/ToggleSwitch'
+export { SearchBar } from './components/SearchBar/SearchBar'
+export { LoadingIndicator } from './components/LoadingIndicator/LoadingIndicator'
+export { SpinnerLoader } from './components/Loader/SpinnerLoader.js'
+export { RingLoader, ButtonLoader } from './components/Loader/RingLoader.js'
+
+export { ToastService } from './services/ToastService/ToastService'
